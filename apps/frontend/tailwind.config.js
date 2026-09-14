7c0624f2561050f7fb652ab39406c895f44fff07
lidiia-pecode <lidiia_tsymborovych@pecodesoftware.com>
2026-09-14 13:01:36 +0300

    chore(frontend): remove the unused Tailwind v3 config

--- apps/frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
